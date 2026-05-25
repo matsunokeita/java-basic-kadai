aca0345663f6d24fe9ef4d899f9488644c51dca7
matsunokeita <matsuno_k@digicomnet.co.jp>
2026-05-25 17:06:31 +0900

    Update DictionaryExec_Chapter21.java

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,17 +1,14 @@
 package kadai_021;
 
 public class DictionaryExec_Chapter21 {
-
-	public static void main(String[] args) {
-		
-		Dictionary_Chapter21 myDictionary = new Dictionary_Chapter21();
-		
-		myDictionary.createDictionary();
-		
-		String[] wordsToSearch = {"apple","banana","grape","orange"};
-		
-		myDictionary.searchWords(wordsToSearch);
-
-	}
-
+    public static void main(String[] args) {
+        
+        Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+        
+        dictionary.createDictionary();
+        
+        String[] targetWords = {"apple", "banana", "grape", "orange"};
+        
+        dictionary.searchWords(targetWords);
+    }
 }
